f5b92fe8787ba843111513ca07167d6440eb2148
Kathleen <kayjsmyth@gmail.com>
2016-11-18 13:22:04 -0500

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,25 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ui.router'])
+    .config(function($stateProvider){
+    	$stateProvider
+    		.state('home', {
+    			url: '/',
+    			templateUrl: 'views/home.html',
+    			controller: 'HomeController'
+    		})
+    		.state('home.notifications', {
+    			url: 'notifications',
+    			templateUrl: 'views/home/notifications.html',
+    			controller: 'NotificationsHomeController'
+    		})
+    		.state('home.timeline', {
+    			url: 'timeline',
+    			templateUrl: 'views/home/timeline.html',
+    			controller: 'TimelineHomeController'
+    		})
+    		.state('home.user', {
+    			url: 'user',
+    			templateUrl: 'views/home/user.html',
+    			controller: 'UserHomeController'
+    		});
+    });
